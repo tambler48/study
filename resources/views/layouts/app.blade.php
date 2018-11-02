b8052e14d8e9f0f626e7c0d5723a43e79fb916b6
Николай <bestbox95@gmail.com>
2018-11-02 14:19:45 +0300

    Подготовка к внедреню ролей Изменены миграции, добавлена миграция для таблицы ролей, удалён неиспользуемый пакет passport. Добавлены посевы начальных данных.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -118,7 +118,7 @@
         </div>
         <!-- /.container -->
     </footer>
-</div>
+    </div>
     <!-- Bootstrap core JavaScript -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
