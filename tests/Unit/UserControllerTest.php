b167380785394ab02fe0b9be7bef2e7cc54cd691
Николай <bestbox95@gmail.com>
2018-11-28 13:42:01 +0300

    Юнит тесты

---
+++ tests/Unit/UserControllerTest.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Николай
+ * Date: 26.11.2018
+ * Time: 12:13
+ */
+
+namespace Tests\Unit;
+
+use App\Http\Controllers\UserController;
+use Tests\TestCase;
+use Illuminate\Support\Facades\DB;
+use Mockery;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\User;
+use App\Role;
+use Gate;
+use Lang;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Route;
+
+
+class UserControllerTest extends TestCase
+{
+   /* public function setUp()
+    {
+        $this->_testable = new Testable();
+    }*/
+
+    public function testUsers(){
+
+        dd(DB::table('users')->insert([
+            'name' => 'testUser1',
+            'role_id' => '1',
+            'active' => true,
+            'email' => 'wwe@test.user',
+            'password' => bcrypt('12345q'),
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s"),
+        ]));
+
+
+    }
+
+    public function testStore()
+    {
+
+        $this->user = new User();
+        $this->user = $this->user::find(1);
+        $this->actingAs($this->user);
+
+
+        $request=$this->post('user/manage', [
+            "_token" => csrf_field(),
+            "name" => "New User",
+            "email" => "7b2qdasad@mail.com",
+            "password" => "12345q",
+            "password_confirmation" => "12345q",
+            "role_id" => "3",
+        ]);
+        $request = Request::shouldReceive();
+
+        dd($request);
+        $userContr = new UserController();
+        dd($userContr->store($request));
+        /*
+               [
+                   "name" => "New User",
+                   "email" => "7b2qdasad@mail.com",
+                   "password" => "12345q",
+                   "password_confirmation" => "12345q",
+                   "role_id" => "3",
+               ]
+               */
+    }
+
+    /**
+     * @dataProvider showProvider
+     * @param $authUser
+     * @param $showUser
+     * @param $answer
+     */
+    public function testShow($authUser, $showUser, $answer)
+    {
+        $this->user = new User();
+        $this->user = $this->user::find($authUser);
+        $this->actingAs($this->user);
+
+        $userContr = new UserController();
+        $this->assertInstanceOf($answer, $userContr->show($showUser));
+    }
+
+    /**
+     *  Data provider for testStore method
+     */
+    public static function storeProvider()
+    {
+        return [
+            [  ],
+        ];
+    }
+
+    /**
+     *  Data provider for testShow method
+     */
+    public static function showProvider()
+    {
+        return [
+            ['1', '1', 'Illuminate\View\View',],
+            ['1', '100', 'Illuminate\Http\RedirectResponse',],
+            ['3', '100', 'Illuminate\Http\RedirectResponse',],
+            ['3', '1', 'Illuminate\Http\RedirectResponse',],
+        ];
+    }
+
+}
